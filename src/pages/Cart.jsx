f15ce0f2d051e41d3fb78e46555fc13579e39d8c
sashaboi <onkarcool123@gmail.com>
2022-03-24 23:53:52 +0530

    cart counter tried

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,14 +3,14 @@ import {useCart} from '../cartcontext'
 
 const cart = () => {
   const {state,dispatch} = useCart();
-  console.log(state)
-  const reducer = (acc,val) =>acc = acc+val.price
-  const total = state.reduce(reducer,0)
+  var total = 0
+  state.map((obj)=>{total = total + obj.price})
+  
   return (
     <div>cart
       {state.length && state.map((obj) => <li>{obj.title} {obj.count} </li>)}
         <button onClick={()=>{dispatch({type:"clearcart"})}} >clear cart</button>
-        <h3>Cart Total : {total}</h3>
+        <h3>Cart Total :{total}</h3>
     </div>
   )
 }
